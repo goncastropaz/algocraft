9adf74aea2d2871eb90812ebeb5d9603e97f939c
vickyperezbustos <vickyperezbustos@gmail.com>
2015-06-03 16:30:02 -0300

    nuevos cambios

--- src/fiuba/algo3/classes/game/Cell.java
+++ src/fiuba/algo3/classes/game/Cell.java
@@ -2,11 +2,11 @@ package fiuba.algo3.classes.game;
 
 public class Cell {
 
-	private String area = "tierra"; //por defaul, depsues podemos separarlas en clases de ultima
+	private String tipoDeArea = "tierra"; //por defaul, depsues podemos separarlas en clases de ultima
 	
 	
 	public void asignarArea(String area){
-		this.area= area;
+		this.tipoDeArea= area;
 		
 	}
 	
--- src/fiuba/algo3/classes/game/GameMap.java
+++ src/fiuba/algo3/classes/game/GameMap.java
@@ -4,9 +4,10 @@ public class GameMap {
 
 	private int size; 
 	private Cell[][] map;  //matriz de tamanio dado por 'size'
+	private Cell[] playerBase;
 	
-	public void GameMap(int cantBases){
-			
+	public GameMap(int cantBases){
+			//cantBases debe ser mayor a 2.
 			this.size = this.getMapSize(cantBases);
 			map= new Cell[this.size][this.size];
 		
@@ -18,6 +19,7 @@ public class GameMap {
 			}
 			this.asignarAreas();
 			this.asignarBases(cantBases);
+			this.setBasePlayers();
 	}
 	
 	private int getMapSize(int cantBases){
@@ -29,6 +31,18 @@ public class GameMap {
 	}
 	private void asignarBases(int cantBases){
 		//se le asigna equidistantemente las bases al mapa.
+	}
+	
+	private void setBasePlayers(){
+		playerBase = new Cell[2];
+		int colFil = this.size /4 -1;
+		playerBase[0] = map[colFil][colFil];
+		colFil = this.size - (this.size/4);
+		playerBase[1] =  map[colFil][colFil];
+	}
+	
+	public Cell getBasePlayer(int numPlayer){
+		return playerBase[numPlayer-1];
 	}
 	
 }
