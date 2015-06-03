a8576ef51b7287ca49897e5359e2bdb6c294b885
vickyperezbustos <vickyperezbustos@gmail.com>
2015-06-03 19:23:51 -0300

    Game, Cell, Building

--- src/fiuba/algo3/classes/game/Game.java
+++ src/fiuba/algo3/classes/game/Game.java
@@ -1,6 +1,38 @@
 package fiuba.algo3.classes.game;
 
+import java.util.HashMap;
+
 public class Game {
+      private HashMap<String,Player> players;
       
-	
+      public Game(){
+    	  String namePlayer = getNombrePlayer("");
+    	  String colorPlayer = getColorPlayer("");
+    	  Race razaPlayer = getRacePlayer();
+    	  Player player1 = new Player(namePlayer,colorPlayer,razaPlayer);
+    	  this.players = new HashMap<String,Player>();
+    	  this.players.put(namePlayer, player1);
+    	  namePlayer = getNombrePlayer(namePlayer);
+    	  colorPlayer = getColorPlayer(colorPlayer);
+    	  razaPlayer = getRacePlayer();
+    	  Player player2 = new Player(namePlayer,colorPlayer,razaPlayer);
+    	  this.players.put(namePlayer, player2);
+    	  
+      }
+
+	private Race getRacePlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String getColorPlayer(String colorNoPermitido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String getNombrePlayer(String nombreNoPermitido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+      
 }
---
+++ src/fiuba/algo3/classes/game/buildings/Building.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.classes.game.buildings;
+
+public class Building {
+
+}
