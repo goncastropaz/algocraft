67db37c20ddfd8ec73fe5cbc222b86a7730d333f
quimey <quimey@quimey-laptop>
2015-06-15 23:40:50 -0300

    Construcciones PuertoEstelar de ambas razas.

---
+++ src/fiuba/algo3/algocraft/construcciones/PuertoEstelarTerran.java
@@ -0,0 +1,22 @@
+package fiuba.algo3.algocraft.construcciones;
+
+import fiuba.algo3.algocraft.excepciones.FueraDeMatriz;
+import fiuba.algo3.classes.stats.Posicion;
+
+public class PuertoEstelarTerran  extends Construccion{
+
+	private static final String  NOMBRE = "PUERTO_ESTELAR_TERRAN";
+	private static final Integer COSTO_MINERAL = 150;
+	private static final Integer COSTO_GAS = 100;
+	private static final Integer TIEMPO_CONSTRUCCION = 10;
+	private static final Integer MAX_VIDA = 1300;
+	private static final Integer ESCUDO = 0;
+	private static final String  UNIDAD = "";
+	
+	
+	public PuertoEstelarTerran(Posicion posicion) throws FueraDeMatriz {
+		super(NOMBRE, COSTO_MINERAL, COSTO_GAS, TIEMPO_CONSTRUCCION, MAX_VIDA,
+				ESCUDO, UNIDAD, posicion);
+	}
+
+}
