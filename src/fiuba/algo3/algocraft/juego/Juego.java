8e82482437cf3e9f85a73f7d737bf4ef18f9d1e2
vickyperezz <vickyperezbustos@gmail.com>
2015-06-20 16:46:40 -0300

    Update Juego.java

--- src/fiuba/algo3/algocraft/juego/Juego.java
+++ src/fiuba/algo3/algocraft/juego/Juego.java
@@ -10,7 +10,7 @@ import fiuba.algo3.algocraft.excepciones.NombreYaExiste;
 
 public class Juego {
 
-    private static Juego INSTANCE = null;
+	private static Juego INSTANCE = null;
     private HashMap<Integer,Jugador> jugadores;
     private Mapa mapaJuego;
     private Turno turno;
@@ -27,51 +27,51 @@ public class Juego {
     }
 
       
-      	private synchronized static void createInstance() throws FueraDeMatriz {
-		if (INSTANCE == null) { 
-			INSTANCE = new Juego();
+      private synchronized static void createInstance() throws FueraDeMatriz {
+			if (INSTANCE == null) { 
+		       INSTANCE = new Juego();
+		    }
 		}
-	}
-		
-      	public static Juego getInstance() throws FueraDeMatriz {
-		if (INSTANCE == null) 
-			createInstance();
-		return INSTANCE;
-	}
+      public static Juego getInstance() throws FueraDeMatriz {
+		    if (INSTANCE == null) 
+		    	createInstance();
+		    return INSTANCE;
+		}
       
-      	public Jugador getJugador(int jugador) throws JugadorInvalido{
+      public Jugador getJugador(int jugador) throws JugadorInvalido{
     	  
     	  	if(jugador>2 || jugador <1) throw new JugadorInvalido();
-		return jugadores.get(jugador);
-	}
-      
-	public void setNombreJugador(int jugador, String nombre) throws NombreYaExiste, JugadorInvalido, NombreConMenosDe4Caracteres{
-				
-		for ( Integer key : this.jugadores.keySet() ){
-			if(this.getJugador(key).getNombre() == nombre) throw new NombreYaExiste();
+			return jugadores.get(jugador);
 		}
-		this.getJugador(jugador).setNombre(nombre);
+      
+		public void setNombreJugador(int jugador, String nombre) throws NombreYaExiste, JugadorInvalido, NombreConMenosDe4Caracteres{
+				
+			for ( Integer key : this.jugadores.keySet() ){
+				if(this.getJugador(key).getNombre() == nombre) throw new NombreYaExiste();
+			}
+			this.getJugador(jugador).setNombre(nombre);
 			
 			
-	}
+		}
 		
-	public void setColorJugador(int jugador, String color) throws JugadorInvalido, ColorYaExiste{
+		public void setColorJugador(int jugador, String color) throws JugadorInvalido, ColorYaExiste{
 
 			
-		for ( Integer key : this.jugadores.keySet() ){
-			if(this.getJugador(key).getColor() == color) throw new ColorYaExiste();
+			for ( Integer key : this.jugadores.keySet() ){
+				if(this.getJugador(key).getColor() == color) throw new ColorYaExiste();
+			}
+			this.getJugador(jugador).setColor(color);
+			
+			
 		}
-		this.getJugador(jugador).setColor(color);
-	}
 	
 
-	public Celda getActualCelda(){
-		return turno.getActualCelda();
-	}
+		public Celda getActualCelda(){
+			return turno.getActualCelda();
+		}
 		
-	public Jugador getActualJugador(){
-		return turno.getActualJugador();
-	}
+		public Jugador getActualJugador(){
+			return turno.getActualJugador();
+		}
 		
-
-}
+	}
