cea39a9d3845981ad796ac6e10a5c08fea2ab2e2
Alejandrad <quimey.11@gmail.com>
2015-06-23 12:40:20 -0300

    cambios vista inicial y controlador del juego

--- src/fiuba/algo3/algocraft/control/ControlJuego.java
+++ src/fiuba/algo3/algocraft/control/ControlJuego.java
@@ -3,27 +3,52 @@ package fiuba.algo3.algocraft.control;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JTextField;
+
 import fiuba.algo3.algocraft.acciones.PasarTurno;
+import fiuba.algo3.algocraft.excepciones.ColorYaExiste;
 import fiuba.algo3.algocraft.excepciones.FueraDeMatriz;
+import fiuba.algo3.algocraft.excepciones.JugadorInvalido;
+import fiuba.algo3.algocraft.excepciones.NombreConMenosDe4Caracteres;
+import fiuba.algo3.algocraft.excepciones.NombreYaExiste;
 import fiuba.algo3.algocraft.juego.Juego;
+import fiuba.algo3.algocraft.vista.VistaInicio;
 
 public class ControlJuego {
 	private Juego juego;
+	private ControlMapa controlMapa;
+	private VistaInicio vistaInicio;
+	
 
-	// VistaJuego vista;
-
-	// public ControlJuego(VistaJuego vista) {
-	// vista = vista;
-	// }
-
-	public void iniciarVista() {
-
+	public ControlJuego(VistaInicio vista) {
+		vistaInicio = vista;
 	}
 
 	private class EscuchaBotonIniciarJuego implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			try {
+	
 				juego = Juego.getInstance();
+				try {
+					juego.setNombreJugador(1,vistaInicio.getTextField().getText());
+					juego.setNombreJugador(2,vistaInicio.getTextField_1().getText());
+					//juego.getJugador(1).setRaza()
+					juego.setColorJugador(1, vistaInicio.getColorSeleccionadoJG1());
+					juego.setColorJugador(2, vistaInicio.getColorSeleccionadoJG2());
+					 
+					
+				} catch (NombreYaExiste e1) {
+					vistaInicio.mostrarError("Los nombres no pueden ser iguales");
+				} catch (JugadorInvalido e1) {
+					vistaInicio.mostrarError("Jugador inválido");
+				} catch (NombreConMenosDe4Caracteres e1) {
+					vistaInicio.mostrarError("El nombre debe tener más de 4 caracteres.");
+				}catch (ColorYaExiste e1) {
+					vistaInicio.mostrarError("El color ya existe");
+				}
+				
+				controlMapa = new ControlMapa();
+
 			} catch (FueraDeMatriz e1) {
 				// TODO Por que el juego tira FUERA DE LA MATRIZ?! :-/
 				e1.printStackTrace();
@@ -86,6 +111,16 @@ public class ControlJuego {
 
 	public ActionListener getListenerBotonCrearNuevoJuego() {
 		return new EscuchaBotonCrearNuevoJuego();
+	}
+	
+	private class EscuchaBotonVolver implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+
+	public ActionListener getListenerBotonVolver() {
+		return new EscuchaBotonVolver();
 	}
 
 }
--- src/fiuba/algo3/algocraft/vista/VistaInicio.java
+++ src/fiuba/algo3/algocraft/vista/VistaInicio.java
@@ -1,19 +1,28 @@
-package interfaz;
+package fiuba.algo3.algocraft.vista;
 
 import java.awt.EventQueue;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
 
-public class Inicio {
+import fiuba.algo3.algocraft.control.ControlJuego;
+
+public class VistaInicio {
 
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
+	private ControlJuego control;
+	private JRadioButton raza1JG1;
+	private JRadioButton raza1JG2;
+	private JRadioButton rojoJG1;
+	private JRadioButton rojoJG2;
+
 
 	/**
 	 * Launch the application.
@@ -22,7 +31,7 @@ public class Inicio {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Inicio window = new Inicio();
+					VistaInicio window = new VistaInicio();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,7 +43,7 @@ public class Inicio {
 	/**
 	 * Create the application.
 	 */
-	public Inicio() {
+	public VistaInicio() {
 		initialize();
 	}
 
@@ -42,85 +51,136 @@ public class Inicio {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		control = new ControlJuego(this);
+
 		frame = new JFrame();
-		frame.setBounds(100, 100, 605, 433);
+		frame.setBounds(100, 100, 605, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setBounds(91, 37, 428, 27);
 		frame.getContentPane().add(textField);
 		textField.setColumns(40);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(40);
 		textField_1.setBounds(91, 178, 428, 27);
 		frame.getContentPane().add(textField_1);
-		
-		//Datos Jugador 1
-		
-		JLabel NombreJugador1 = new JLabel("Nombre Jugador 1");
-		NombreJugador1.setBounds(199, 11, 204, 27);
-		frame.getContentPane().add(NombreJugador1);
-		
+
+		// Datos Jugador 1
+
+		JLabel nombreJugador1 = new JLabel("Nombre Jugador 1");
+		nombreJugador1.setBounds(199, 11, 204, 27);
+		frame.getContentPane().add(nombreJugador1);
+
 		ButtonGroup buttonGroupRazaJG1 = new ButtonGroup();
-		
-		JRadioButton raza1JG1 = new JRadioButton("Terran");
+
+		raza1JG1 = new JRadioButton("Terran");
 		raza1JG1.setBounds(103, 75, 109, 23);
 		buttonGroupRazaJG1.add(raza1JG1);
 		frame.getContentPane().add(raza1JG1);
-		
+
 		JRadioButton raza2JG1 = new JRadioButton("Protoss");
 		raza2JG1.setBounds(346, 75, 109, 23);
 		buttonGroupRazaJG1.add(raza2JG1);
 		frame.getContentPane().add(raza2JG1);
-		
+
 		ButtonGroup buttonGroupColorJG1 = new ButtonGroup();
-		
-		JRadioButton RojoJG1 = new JRadioButton("Rojo");
-		RojoJG1.setBounds(103, 106, 55, 23);
-		buttonGroupColorJG1.add(RojoJG1);
-		frame.getContentPane().add(RojoJG1);
-		
-		JRadioButton AzulJG1 = new JRadioButton("Azul");
-		AzulJG1.setBounds(346, 106, 109, 23);
-		buttonGroupColorJG1.add(AzulJG1);
-		frame.getContentPane().add(AzulJG1);
-		
-		//Datos Jugador 2
-		
+
+		rojoJG1 = new JRadioButton("Rojo");
+		rojoJG1.setBounds(103, 106, 55, 23);
+		buttonGroupColorJG1.add(rojoJG1);
+		frame.getContentPane().add(rojoJG1);
+
+		JRadioButton azulJG1 = new JRadioButton("Azul");
+		azulJG1.setBounds(346, 106, 109, 23);
+		buttonGroupColorJG1.add(azulJG1);
+		frame.getContentPane().add(azulJG1);
+
+		// Datos Jugador 2
+
 		JLabel NombreJugador2 = new JLabel("Nombre Jugador 2");
 		NombreJugador2.setBounds(199, 153, 204, 14);
 		frame.getContentPane().add(NombreJugador2);
-		
+
 		ButtonGroup buttonGroupRazaJG2 = new ButtonGroup();
-		
-		JRadioButton raza1JG2 = new JRadioButton("Terran");
+
+		raza1JG2 = new JRadioButton("Terran");
 		raza1JG2.setBounds(103, 232, 109, 23);
 		buttonGroupRazaJG2.add(raza1JG2);
 		frame.getContentPane().add(raza1JG2);
-		
+
 		JRadioButton raza2JG2 = new JRadioButton("Protoss");
 		raza2JG2.setBounds(346, 232, 109, 23);
 		buttonGroupRazaJG2.add(raza2JG2);
 		frame.getContentPane().add(raza2JG2);
-		
+
 		ButtonGroup buttonGroupColorJG2 = new ButtonGroup();
-		
-		JRadioButton RojoJG2 = new JRadioButton("Rojo");
-		RojoJG2.setBounds(103, 263, 55, 23);
-		buttonGroupColorJG2.add(RojoJG2);
-		frame.getContentPane().add(RojoJG2);
-		
+
+		rojoJG2 = new JRadioButton("Rojo");
+		rojoJG2.setBounds(103, 263, 55, 23);
+		buttonGroupColorJG2.add(rojoJG2);
+		frame.getContentPane().add(rojoJG2);
+
 		JRadioButton AzulJG2 = new JRadioButton("Azul");
 		AzulJG2.setBounds(346, 263, 109, 23);
 		buttonGroupColorJG2.add(AzulJG2);
 		frame.getContentPane().add(AzulJG2);
-		
-		
+
 		JButton btnComenzarPartida = new JButton("Comenzar Partida");
 		btnComenzarPartida.setBounds(197, 332, 172, 23);
+		btnComenzarPartida.addActionListener(control
+				.getListenerBotonIniciarJuego());
 		frame.getContentPane().add(btnComenzarPartida);
-	
+
 	}
+
+	public void mostrarError(String string) {
+
+		JOptionPane.showMessageDialog(frame, string);
+
+	}
+	public String getColorSeleccionadoJG1() {
+
+		if (this.rojoJG1.isSelected()) {
+			return "rojo";
+		}
+		return "azul";
+	}
+
+	public String getColorSeleccionadoJG2() {
+
+		if (this.rojoJG2.isSelected()) {
+			return "rojo";
+		}
+		return "azul";
+	}
+
+	public String getRazaSeleccionadaJG1() {
+
+		if (this.raza1JG1.isSelected()) {
+			return raza1JG1.getText();
+		}
+		return raza1JG1.getText();
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
+	}
+
+	public JTextField getTextField_1() {
+		return textField_1;
+	}
+
+	public void setTextField_1(JTextField textField_1) {
+		this.textField_1 = textField_1;
+	}	
+	
+	
 }
