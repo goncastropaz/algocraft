60a5e69cc8688daecd5421e57228e38ace371fcb
TKiller <TKiller@PC>
2015-06-23 23:39:31 -0300

    excepcion correspondiente commit anterior

---
+++ src/fiuba/algo3/algocraft/excepciones/CompletarDatosException.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.algocraft.excepciones;
+
+public class CompletarDatosException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2082140117601502772L;
+
+}
