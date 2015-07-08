837bfb96261b7d5bb281b7802929c864071b032b
TKiller <TKiller@PC>
2015-07-07 21:34:57 -0300

    clases restantes del commit anterior

---
+++ src/fiuba/algo3/modelo/excepciones/MagiaDesconocida.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.modelo.excepciones;
+
+public class MagiaDesconocida extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2594888614382505385L;
+
+}
---
+++ src/fiuba/algo3/modelo/excepciones/UnidadNoTieneMagia.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.modelo.excepciones;
+
+public class UnidadNoTieneMagia extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3229122451318372155L;
+
+}
